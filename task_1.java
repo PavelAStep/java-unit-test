c55ec3b0a2666faaa91d515726ced5e53c5a9b53
Pavel Stepanenko <pavel.a.step@yandex.ru>
2024-11-12 00:05:19 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,9 @@
+   
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	// Напиши код здесь
+    assertEquals("Должно быть больше 18: ", true, isAdult);
+}
+
